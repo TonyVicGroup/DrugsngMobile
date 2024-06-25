05ed6d73ea59218d47a263f2846987b9cccc8b68
Eric Onyeulo <onyeuloeric@gmail.com>
2024-06-25 14:23:32 +0100

    first commit - project setup

---
+++ android/app/src/main/kotlin/com/example/drugs_ng/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.drugs_ng
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/src/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+/// The Widget that configures your application.
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Providing a restorationScopeId allows the Navigator built by the
+      // MaterialApp to restore the navigation stack when a user leaves and
+      // returns to the app after it has been killed while running in the
+      // background.
+      restorationScopeId: 'app',
+    );
+  }
+}
