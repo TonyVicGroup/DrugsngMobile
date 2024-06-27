234f0a016ce38cdfc11e760b1fcadabe1fb89543
Enri-code <onyeuloeric@gmail.com>
2024-06-27 18:54:21 +0100

    add app environment

---
+++ lib/core/utils/environment.dart
@@ -0,0 +1,20 @@
+enum Environment { dev, prod }
+
+abstract class AppEnvironment {
+  static late String baseApiUrl;
+
+  static late Environment _environment;
+  static Environment get value => _environment;
+
+  static setupEnv(Environment env) {
+    _environment = env;
+    switch (env) {
+      case Environment.dev:
+        baseApiUrl = '';
+        break;
+      case Environment.prod:
+        baseApiUrl = '';
+        break;
+    }
+  }
+}
