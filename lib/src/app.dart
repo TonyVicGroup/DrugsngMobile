61bf260b9435c55da67d8f07655c43747800b11c
Enri-code <onyeuloeric@gmail.com>
2024-06-27 09:05:12 +0100

    feat: setup folder structure and add assets
    - setup onboarding folder structure
    - setup project core folder
    - add image and svg for project

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,27 @@
+import 'package:drugs_ng/features/onboarding/presentation/pages/onboarding.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      restorationScopeId: 'app',
-    );
+    return ScreenUtilInit(
+        designSize: const Size(360, 800),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (_, context) {
+          return MaterialApp(
+            restorationScopeId: 'app',
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+              fontFamily: 'Satoshi',
+              // scaffoldBackgroundColor: NbColors.background,
+            ),
+            home: const OnboardingPage(),
+          );
+        });
   }
 }
