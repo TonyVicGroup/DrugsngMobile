a9922ac161b8f5dfb3f494a72fe42e82800e8a8d
Enri-code <onyeuloeric@gmail.com>
2024-07-02 12:58:44 +0100

    create setup for checkout notificaiton and prescription pages

---
+++ lib/src/features/notification/presentation/pages/notification_page.dart
@@ -0,0 +1,34 @@
+import 'package:drugs_ng/src/core/contants/app_color.dart';
+import 'package:drugs_ng/src/core/contants/app_image.dart';
+import 'package:drugs_ng/src/core/ui/app_text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class NotificationPage extends StatelessWidget {
+  const NotificationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        shadowColor: Colors.black.withOpacity(0.2),
+        elevation: 5,
+        surfaceTintColor: AppColor.white,
+        backgroundColor: AppColor.white,
+        leading: Center(
+          child: InkWell(
+            onTap: () => Navigator.pop(context),
+            child: SizedBox(
+              width: 20.r,
+              height: 20.r,
+              child: SvgPicture.asset(AppSvg.chevronThick),
+            ),
+          ),
+        ),
+        title: AppText.sp18("Notifications").w700.black,
+        centerTitle: true,
+      ),
+    );
+  }
+}
