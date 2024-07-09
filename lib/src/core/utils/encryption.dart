bb3226a54e3b050faf4dcbe3b39fc1d492718b51
Eric Onyeulo <onyeuloeric@gmail.com>
2024-07-09 16:32:06 +0100

    chore: fixed encryption logic

--- lib/src/core/utils/encryption.dart
+++ lib/src/core/utils/encryption.dart
@@ -1,7 +1,9 @@
 import 'package:encrypt/encrypt.dart';
 import 'package:pointycastle/asymmetric/api.dart';
+// import 'package:flutter/foundation.dart' as f;
 
 class Encryption {
+  ///Values in base64
   final String key, data;
   const Encryption(this.key, this.data);
 
@@ -13,23 +15,52 @@ class Encryption {
   ///
 
   static final _encryptionIV = IV.fromUtf8('DrugsngPassword');
-
   factory Encryption.encryptString(String value) {
     final key = Key.fromSecureRandom(16);
+    final encrypted = Encrypter(AES(key, mode: AESMode.ecb))
+        .encrypt(value, iv: _encryptionIV);
 
-    final encrypted = Encrypter(AES(key)).encrypt(value, iv: _encryptionIV);
-
-    return Encryption(_encryptWithRSA(key.base64), encrypted.base64);
+    return Encryption(_encryptKey(key), encrypted.base64);
   }
 
   static final _rSAEncrypter = Encrypter(RSA(
-    publicKey: RSAKeyParser().parse(
-      '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAkFpdobZv/tjGzf8xXcXO\nPwk/U3aEgypAi1sgKWnzPglnI7L+5ze91QzDJle6xq0l5KXmM9pnE39N4q4jOqdW\nXW6tefXbbzqZSEW+5bsB1t1Ymu4DUh0XOGlOHKRMlUoAgQYDdYYyo1yQ+eQrFlpC\nw93iPX297Vovgm/qAffeczgUb90NeYkUxqS5G/iPWqkzYbaDWYFK5O5cfDnOf3RR\nCEMeSUsGQ8XHRdyNBBkWY4LsyZh2jDWaYYgVP0/s9xTtMn3q7kaULUgCF0F6r1Z+\nIwry9py7kTRypt2p10zoPB0DAW49lgXT8UuPUXjF2yOcRdUBHvC9CNSppCI0E1uo\nkwIDAQAB\n-----END PUBLIC KEY-----',
-    ) as RSAPublicKey,
-  ));
-
-  static String _encryptWithRSA(String value) {
-    final encrypted = _rSAEncrypter.encrypt(value);
+      publicKey: RSAKeyParser().parse(
+    '''-----BEGIN PUBLIC KEY-----
+MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgGpqQk8BJWGL7qBAWGeAFTEYcsm7
+1vK2YwlPw+2HeqyDPqc4fyg8sCdo6qABXhQPnjWFs49E792QZiwmzEsSMU1ML0vL
+spToThU9kpEYHyc9xObdpQuc3NXF8ilMJWOVOEA16x/HPwMBtnG6f446yUoT2tQg
+qrMgAETknl4FkAjBAgMBAAE=
+-----END PUBLIC KEY-----''',
+  ) as RSAPublicKey));
+  static String _encryptKey(Key key) {
+    final encrypted = _rSAEncrypter.encrypt(key.base64);
     return encrypted.base64;
   }
+
+//   static final _rSA2Encrypter = Encrypter(RSA(
+//     privateKey: RSAKeyParser().parse('''-----BEGIN RSA PRIVATE KEY-----
+// MIICWwIBAAKBgGpqQk8BJWGL7qBAWGeAFTEYcsm71vK2YwlPw+2HeqyDPqc4fyg8
+// sCdo6qABXhQPnjWFs49E792QZiwmzEsSMU1ML0vLspToThU9kpEYHyc9xObdpQuc
+// 3NXF8ilMJWOVOEA16x/HPwMBtnG6f446yUoT2tQgqrMgAETknl4FkAjBAgMBAAEC
+// gYBbBg01+ur4p3M0DBYSYhK+bgUx3cSc07me62XSNYKPMaxT6RWLW23qJ+oZd1H7
+// ouhXK8hNklACm1NqDL3OsP8N3u6G21K1yKdIjAIv0BJ7N/JlE0FtVXt3cCkD3iQE
+// zFs2N8LzURPfShuiKZ8tXUW9cnXhyySFqKKSZsmoyOtL4QJBAKaXK4kCTCNA70NX
+// oiwvkU+Ey1u8N0JhVQhZ57VqIqlUSdMt7q1cHwxNMzjcn3Iv1mifsEduMY3Y7K9U
+// Ft0FpTUCQQCjhz1E0+rvpQgBQBzchsh9vzsE71bD3COgmBnOvOu0g7+lEc7MWPJ0
+// E/bivJMnwVfdqy/5GfETRX+RIE2FjgLdAkAEjCnfEpX7fGFLqE+//whrcEeQ2IF1
+// qWyFztZ5aE1L7AYb4qwaRWJ/lnfofHVJy66BqqJIQOTPZ3WGj7gUDnxlAkEAnCMx
+// 5fjt1lld1kvQAuQSpLYldSXNU39q6Rixc4tBBv/QyZzCNq0q+phhX8asPwZFjhq4
+// 2IDjhQITto8AVeftZQJAKa6Z3mLVaeAmuTjTAGvE8EdA6RFIcq/kmwzdy/B4jSA4
+// biv6nw0XoziSJQbQEf8Uiibl3V8CT8YSkt1L5ON3Hg==
+// -----END RSA PRIVATE KEY-----''') as RSAPrivateKey,
+//   ));
+
+//   @f.visibleForTesting
+//   String decrypt() {
+//     final key = _rSA2Encrypter.decrypt64(this.key);
+//     final data = Encrypter(AES(Key.fromBase64(key), mode: AESMode.ecb))
+//         .decrypt64(this.data, iv: _encryptionIV);
+
+//     return data;
+//   }
 }
